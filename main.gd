cd80d417cb977216e45718a38ecbda4dcc5b873d
Nathan Lessard <lessard.nathan@carrefour.cegepvicto.ca>
2020-11-03 17:23:36 -0500

    Parcours en profondeur ajouter au projet

--- main.gd
+++ main.gd
@@ -53,20 +53,6 @@ func _ready():
 	for i in graph:
 		noeud.append(get_node("noeud" + str(i)))
 
-# Parcours en largeur le labyrinthe
-func _on_parcourir_pressed():
-	parcours.push_front(graph[0])
-	estVisite.append(0)
-	
-	while !parcours.empty():
-		var noeudPresent = parcours.pop_back()
-		for voisin in noeudPresent:
-			if !estVisite.has(voisin):
-				if noeud[voisin].couleur != Color(0.607843, 0.607843, 0.607843, 1):
-					estVisite.append(voisin)
-					parcours.push_front(graph[voisin])
-	$runTime.start(1.0)
-
 # Vérifie si l'array de visite est vide 
 # Sinon enlève le premier noeud de l'array visite
 # et lui assigne la couleurVisite
@@ -80,3 +66,40 @@ func _on_runTime_timeout():
 # Redonne la couleur de base à chacun des noeuds.
 func _on_recommencer_pressed():
 	get_tree().call_group("noeuds", "_setCouleurBase")
+
+# Parcours en largeur le labyrinthe
+func _on_largeur_pressed():
+	parcours.push_front(graph[0])
+	estVisite.append(0)
+	
+	while !parcours.empty():
+		var noeudPresent = parcours.pop_back()
+		for voisin in noeudPresent:
+			if !estVisite.has(voisin):
+				if noeud[voisin].couleur != Color(0.607843, 0.607843, 0.607843, 1):
+					estVisite.append(voisin)
+					parcours.push_front(graph[voisin])
+	# Après avoir marqué tous les voisin faire commencé le timer qui va stopper 1 seconde
+	# entre chaque execution de la fonction lier _on_runTime_timeout
+	$runTime.start(1.0)
+
+# Appelle la fonction récursive pour parcourir le labyrinthe en profondeur
+# Commence en appelant le premier noeud (La case bleu du début)
+func _on_profondeur_pressed():
+	# Appeler la fonction récursive pour la première case.
+	_profondeur_visite(0)
+	# Après avoir marqué tous les voisin faire commencé le timer qui va stopper 1 seconde
+	# entre chaque execution de la fonction lier _on_runTime_timeout
+	$runTime.start(1.0)
+
+# Parcours récursif du labyrinthe
+func _profondeur_visite(present):
+	estVisite.append(present)
+	
+	# Parcours chaque case voisine
+	# Si la case est déjà visité essaye la prochaine sinon vérifie que ce n'est
+	# pas un mur selon la couleur de la case.
+	for voisin in graph[present]:
+		if !estVisite.has(voisin):
+			if noeud[voisin].couleur != Color(0.607843, 0.607843, 0.607843, 1):
+				_profondeur_visite(voisin)
