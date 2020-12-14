eb9ccd0598955589567fadce26ce045fbab9bde6
Nathan Lessard <lessard.nathan@carrefour.cegepvicto.ca>
2020-12-14 10:09:47 -0500

    Labyrinthe du précedent travail modifier pour le labyrinthe du travail final

--- main.gd
+++ main.gd
@@ -2,14 +2,28 @@ extends Node
 
 # Déclaration des constantes
 
+# type=0 -> plancher
+# type=1 -> mur
+# type=2 -> petit méchant
+# type=3 -> gros méchant
+# type=4 -> nourriture
+# type=5 -> arme
+# type=6 -> fin
+# type=7 -> joueur
+# Dictionnaire des poids des différents types
+const poids = {
+	0:4,
+	1:500,
+	2:25,
+	3:50,
+	4:3,
+	5:2,
+	6:1,
+	7:4
+}
+
 # Dictionnaire des noeuds
 # Chaque index du dictionnaire est un noeud
-# Les array que l'index contient est l'index de son voisin
-# Ce n'est pas lier au noeud, mais pour changer la disposition du labyrinthe
-# il faut que l'on change la couleur des case dans la scène 2d en cliquant sur
-# la case et ensuite sur base couleur pour un mur set la couleur à 
-# Color(0.607843, 0.607843, 0.607843, 1)
-# la case 9 est la fin du labyrinthe
 const graph = {
 	0:[1,5],
 	1:[0,2,6],
@@ -38,9 +52,6 @@ const graph = {
 	24:[19,23]
 }
 
-# La couleur que la case change après avoir été visité
-const couleurVisite = Color(1, 0.84, 0, 1)
-
 # Déclaration des variables
 
 # Un array des noeuds du labyrinthe
@@ -48,15 +59,12 @@ const couleurVisite = Color(1, 0.84, 0, 1)
 # Aussi c'est avec cette var que le noeud va changer de couleur
 var noeud = []
 
-# Sert à savoir le dernier noeud parcouru
-var parcours = []
-
 # Sert à savoir si le noeud a été parcouru
 var estVisite = []
 
 # Sert à savoir si le noeud a été parcouru dans le chemin le plus court
 var distance = {
-	0:0,
+	0:100000000,
 	1:100000000,
 	2:100000000,
 	3:100000000,
@@ -85,7 +93,7 @@ var distance = {
 
 # Dictionnaire des chemin pour retenir le chemin
 var chemin = {
-	0:[0],
+	0:[],
 	1:[],
 	2:[],
 	3:[],
@@ -112,78 +120,121 @@ var chemin = {
 	24:[]
 }
 
+# Pour savoir l'index de la première case
+var debut
+
+# Pour savoir l'index de la dernière case
+var fin
+
 # Pour l'algo de dijkstra array pour savoir si le noeud a été visiter
 var marquer = []
 
 # Retiens la prochaine case qui sera envoyer dans l'algo de dijkstra
 var prochain
 
+# Retiens la case visité en dernier par le joeur
+var lastCase
+
+# Random
+var rng = RandomNumberGenerator.new()
+
+onready var tmap = $VBoxContainer/tmap
+
 # Remplie l'array de noeud
 func _ready():
 	for i in graph:
 		noeud.append(get_node("noeud" + str(i)))
+	
+	randomMap()
 
-# Vérifie si l'array de visite est vide 
-# Sinon enlève le premier noeud de l'array visite
-# et lui assigne la couleurVisite
+# Vérifie si l'array de visite est vide et set couleur
 func _on_runTime_timeout():
 	if estVisite.empty():
 		$runTime.stop()
 	else:
 		var i = estVisite.pop_front()
-		noeud[i]._setCouleur(couleurVisite)
+		noeud[i].setMouvement()
+		if lastCase != null:
+			noeud[lastCase].setBack()
+		lastCase = i;
 
 # Redonne la couleur de base à chacun des noeuds.
 func _on_recommencer_pressed():
-	get_tree().call_group("noeuds", "_setCouleurBase")
-
-# Parcours en largeur le labyrinthe
-func _on_largeur_pressed():
-	parcours.push_front(graph[0])
-	estVisite.append(0)
+	distance = {
+		0:100000000,
+		1:100000000,
+		2:100000000,
+		3:100000000,
+		4:100000000,
+		5:100000000,
+		6:100000000,
+		7:100000000,
+		8:100000000,
+		9:100000000,
+		10:100000000,
+		11:100000000,
+		12:100000000,
+		13:100000000,
+		14:100000000,
+		15:100000000,
+		16:100000000,
+		17:100000000,
+		18:100000000,
+		19:100000000,
+		20:100000000,
+		21:100000000,
+		22:100000000,
+		23:100000000,
+		24:100000000
+	}
 	
-	while !parcours.empty():
-		var noeudPresent = parcours.pop_back()
-		for voisin in noeudPresent:
-			if !estVisite.has(voisin):
-				if noeud[voisin].couleur != Color(0.607843, 0.607843, 0.607843, 1):
-					estVisite.append(voisin)
-					parcours.push_front(graph[voisin])
-	# Après avoir marqué tous les voisin faire commencé le timer qui va stopper 1 seconde
-	# entre chaque execution de la fonction lier _on_runTime_timeout
-	$runTime.start(1.0)
-
-# Appelle la fonction récursive pour parcourir le labyrinthe en profondeur
-# Commence en appelant le premier noeud (La case bleu du début)
-func _on_profondeur_pressed():
-	# Appeler la fonction récursive pour la première case.
-	_profondeur_visite(0)
-	# Après avoir marqué tous les voisin faire commencé le timer qui va stopper 1 seconde
-	# entre chaque execution de la fonction lier _on_runTime_timeout
-	$runTime.start(1.0)
-
-# Parcours récursif du labyrinthe
-func _profondeur_visite(present):
-	estVisite.append(present)
+	marquer = []
 	
-	# Parcours chaque case voisine
-	# Si la case est déjà visité essaye la prochaine sinon vérifie que ce n'est
-	# pas un mur selon la couleur de la case.
-	for voisin in graph[present]:
-		if !estVisite.has(voisin):
-			if noeud[voisin].couleur != Color(0.607843, 0.607843, 0.607843, 1):
-				_profondeur_visite(voisin)
+	chemin = {
+		0:[],
+		1:[],
+		2:[],
+		3:[],
+		4:[],
+		5:[],
+		6:[],
+		7:[],
+		8:[],
+		9:[],
+		10:[],
+		11:[],
+		12:[],
+		13:[],
+		14:[],
+		15:[],
+		16:[],
+		17:[],
+		18:[],
+		19:[],
+		20:[],
+		21:[],
+		22:[],
+		23:[],
+		24:[]
+	}
+	
+	estVisite = []
+	
+	lastCase = null
+	randomMap()
+
+
 
 # À la suite de l'action sur le bouton remplie l'array des noeud marquer 
 # non-visiter ensuite appelle la fonction récursive pour trouver le chemin
-# ensuite marque le meilleur chemin pour se rendre à la case 9(la case rouge)
-func _on_dijkstra_pressed():
+# ensuite marque le meilleur chemin pour se rendre à la de type 7
+func _on_parcourir_pressed():
 	for i in graph:
 		marquer.append(i)
 	
-	_dijkstra_visite(0)
+	_dijkstra_visite(debut)
 	
-	for i in chemin[9]:
+	for i in chemin[fin]:
 		estVisite.append(i)
 	# Après avoir marqué le chemin faire commencé le timer qui va stopper 1 seconde
 	# entre chaque execution de la fonction lier _on_runTime_timeout
@@ -192,9 +243,6 @@ func _on_dijkstra_pressed():
 # Fonction récursive pour trouvé le chemin le plus court
 # prends en paramètre la case qui présente qui sera enlever de marquer
 func _dijkstra_visite(present):
-	# Le poid entre les case
-	var poid
-	
 	# La distance de la prochaine case
 	var distProchain = 100000000
 	
@@ -202,14 +250,9 @@ func _dijkstra_visite(present):
 	# c'est un mur ou une case normal
 	for voisin in graph[present]:
 		if marquer.has(voisin):
-			if noeud[voisin].couleur != Color(0.607843, 0.607843, 0.607843, 1):
-				poid = 1
-			else:
-				poid = 500
-			
 			# calcul si la case doit réduire sa distance
-			if distance[voisin] > distance[present] + poid:
-				distance[voisin] = distance[present] + poid
+			if distance[voisin] > distance[present] + poids[noeud[voisin].type]:
+				distance[voisin] = distance[present] + poids[noeud[voisin].type]
 				
 				# après avoir réduit la distance lui donne son nouveau chemin
 				# le plus court en oubliant l'ancien.
@@ -241,3 +284,189 @@ func _dijkstra_visite(present):
 			_dijkstra_visite(prochain)
 		else:
 			_dijkstra_visite(marquer.front())
+
+func randomMap():
+	rng.randomize()
+	debut = rng.randi_range(0,9)
+	
+	rng.randomize()
+	fin = rng.randi_range(15,24)
+	
+	chemin[debut].append(debut)
+	
+	distance[debut] = 0
+	
+	rng.randomize()
+	var map = rng.randi_range(0,4)
+	
+	tmap.set_text(str('Le labyrinthe utilisé est produit avec la map ', map+1))
+	
+	if map == 0:
+		noeud[0].setNoeud(0)
+		noeud[1].setNoeud(0)
+		noeud[2].setNoeud(1)
+		noeud[3].setNoeud(0)
+		noeud[4].setNoeud(0)
+		noeud[5].setNoeud(1)
+		noeud[6].setNoeud(0)
+		noeud[7].setNoeud(1)
+		noeud[8].setNoeud(1)
+		noeud[9].setNoeud(0)
+		noeud[10].setNoeud(0)
+		noeud[11].setNoeud(0)
+		noeud[12].setNoeud(1)
+		noeud[13].setNoeud(1)
+		noeud[14].setNoeud(0)
+		noeud[15].setNoeud(0)
+		noeud[16].setNoeud(1)
+		noeud[17].setNoeud(0)
+		noeud[18].setNoeud(0)
+		noeud[19].setNoeud(0)
+		noeud[20].setNoeud(0)
+		noeud[21].setNoeud(0)
+		noeud[22].setNoeud(0)
+		noeud[23].setNoeud(0)
+		noeud[24].setNoeud(1)
+	elif map == 1:
+		noeud[0].setNoeud(1)
+		noeud[1].setNoeud(0)
+		noeud[2].setNoeud(1)
+		noeud[3].setNoeud(0)
+		noeud[4].setNoeud(1)
+		noeud[5].setNoeud(0)
+		noeud[6].setNoeud(0)
+		noeud[7].setNoeud(1)
+		noeud[8].setNoeud(0)
+		noeud[9].setNoeud(0)
+		noeud[10].setNoeud(1)
+		noeud[11].setNoeud(0)
+		noeud[12].setNoeud(1)
+		noeud[13].setNoeud(0)
+		noeud[14].setNoeud(0)
+		noeud[15].setNoeud(0)
+		noeud[16].setNoeud(0)
+		noeud[17].setNoeud(0)
+		noeud[18].setNoeud(0)
+		noeud[19].setNoeud(1)
+		noeud[20].setNoeud(1)
+		noeud[21].setNoeud(0)
+		noeud[22].setNoeud(1)
+		noeud[23].setNoeud(0)
+		noeud[24].setNoeud(0)
+	elif map == 2:
+		noeud[0].setNoeud(0)
+		noeud[1].setNoeud(1)
+		noeud[2].setNoeud(0)
+		noeud[3].setNoeud(0)
+		noeud[4].setNoeud(0)
+		noeud[5].setNoeud(0)
+		noeud[6].setNoeud(1)
+		noeud[7].setNoeud(1)
+		noeud[8].setNoeud(1)
+		noeud[9].setNoeud(0)
+		noeud[10].setNoeud(0)
+		noeud[11].setNoeud(0)
+		noeud[12].setNoeud(0)
+		noeud[13].setNoeud(1)
+		noeud[14].setNoeud(0)
+		noeud[15].setNoeud(0)
+		noeud[16].setNoeud(1)
+		noeud[17].setNoeud(1)
+		noeud[18].setNoeud(1)
+		noeud[19].setNoeud(0)
+		noeud[20].setNoeud(0)
+		noeud[21].setNoeud(0)
+		noeud[22].setNoeud(0)
+		noeud[23].setNoeud(0)
+		noeud[24].setNoeud(0)
+	elif map == 3:
+		noeud[0].setNoeud(0)
+		noeud[1].setNoeud(0)
+		noeud[2].setNoeud(0)
+		noeud[3].setNoeud(1)
+		noeud[4].setNoeud(0)
+		noeud[5].setNoeud(0)
+		noeud[6].setNoeud(1)
+		noeud[7].setNoeud(1)
+		noeud[8].setNoeud(1)
+		noeud[9].setNoeud(0)
+		noeud[10].setNoeud(0)
+		noeud[11].setNoeud(0)
+		noeud[12].setNoeud(0)
+		noeud[13].setNoeud(1)
+		noeud[14].setNoeud(0)
+		noeud[15].setNoeud(0)
+		noeud[16].setNoeud(1)
+		noeud[17].setNoeud(1)
+		noeud[18].setNoeud(1)
+		noeud[19].setNoeud(0)
+		noeud[20].setNoeud(0)
+		noeud[21].setNoeud(0)
+		noeud[22].setNoeud(0)
+		noeud[23].setNoeud(0)
+		noeud[24].setNoeud(0)
+	elif map == 4:
+		noeud[0].setNoeud(0)
+		noeud[1].setNoeud(0)
+		noeud[2].setNoeud(0)
+		noeud[3].setNoeud(1)
+		noeud[4].setNoeud(0)
+		noeud[5].setNoeud(0)
+		noeud[6].setNoeud(1)
+		noeud[7].setNoeud(1)
+		noeud[8].setNoeud(1)
+		noeud[9].setNoeud(0)
+		noeud[10].setNoeud(0)
+		noeud[11].setNoeud(1)
+		noeud[12].setNoeud(0)
+		noeud[13].setNoeud(0)
+		noeud[14].setNoeud(0)
+		noeud[15].setNoeud(0)
+		noeud[16].setNoeud(1)
+		noeud[17].setNoeud(1)
+		noeud[18].setNoeud(1)
+		noeud[19].setNoeud(0)
+		noeud[20].setNoeud(0)
+		noeud[21].setNoeud(0)
+		noeud[22].setNoeud(0)
+		noeud[23].setNoeud(0)
+		noeud[24].setNoeud(0)
+	else:
+		noeud[0].setNoeud(0)
+		noeud[1].setNoeud(1)
+		noeud[2].setNoeud(0)
+		noeud[3].setNoeud(0)
+		noeud[4].setNoeud(0)
+		noeud[5].setNoeud(0)
+		noeud[6].setNoeud(1)
+		noeud[7].setNoeud(1)
+		noeud[8].setNoeud(1)
+		noeud[9].setNoeud(0)
+		noeud[10].setNoeud(0)
+		noeud[11].setNoeud(1)
+		noeud[12].setNoeud(0)
+		noeud[13].setNoeud(0)
+		noeud[14].setNoeud(0)
+		noeud[15].setNoeud(0)
+		noeud[16].setNoeud(1)
+		noeud[17].setNoeud(1)
+		noeud[18].setNoeud(1)
+		noeud[19].setNoeud(0)
+		noeud[20].setNoeud(0)
+		noeud[21].setNoeud(0)
+		noeud[22].setNoeud(0)
+		noeud[23].setNoeud(0)
+		noeud[24].setNoeud(0)
+	
+	for i in graph:
+		rng.randomize()
+		var type = rng.randi_range(0,5)
+		
+		if i == debut:
+			noeud[i].setNoeud(7)
+		elif i == fin:
+			noeud[i].setNoeud(6)
+		else:
+			if noeud[i].type == 0:
+				if type != 1:
+					noeud[i].setNoeud(type)
--- noeud.gd
+++ noeud.gd
@@ -1,19 +1,61 @@
 extends Node2D
 
+# Déclaration des constante
+# type=0 -> plancher
+# type=1 -> mur
+# type=2 -> petit méchant
+# type=3 -> gros méchant
+# type=4 -> nourriture
+# type=5 -> arme
+# type=6 -> fin
+# type=7 -> joueur
+# Dictionnaire des couleurs des différents types
+const couleurs = {
+	0:Color(1,1,1,1),
+	1:Color(0,0,0,1),
+	2:Color(1,1,1,1),
+	3:Color(1,1,1,1),
+	4:Color(1,1,1,1),
+	5:Color(1,1,1,1),
+	6:Color(0,1,1,1),
+	7:Color(0,0.55,0.55,1)
+}
+
 # Déclaration des variables
-var couleur
-export(Color) var baseCouleur
+var imgArme = Image.new()
+var itexArme = ImageTexture.new()
+var imgNour = Image.new()
+var itexNour = ImageTexture.new()
+var imgPetit = Image.new()
+var itexPetit = ImageTexture.new()
+var imgGros = Image.new()
+var itexGros = ImageTexture.new()
+var imgJoueur = Image.new()
+var itexJoueur = ImageTexture.new()
+var itex
+var type
 onready var noeudUtilise = $Polygon2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	couleur = baseCouleur
-	noeudUtilise.color = couleur
+	itex = {
+		0:null,
+		1:null,
+		2:load("res://petit-monstre.png"),
+		3:load("res://gros-monstre.png"),
+		4:load("res://soupe.png"),
+		5:load("res://arme.png"),
+		6:null,
+		7:load("res://joueur.png")
+	}
 
-func _setCouleur(c):
-	couleur = c
-	noeudUtilise.color = couleur
 
-func _setCouleurBase():
-	couleur = baseCouleur
-	noeudUtilise.color = couleur
+func setMouvement():
+	noeudUtilise.texture = itex[7]
+
+func setBack():
+	noeudUtilise.texture = itex[0]
+
+func setNoeud(t):
+	type = t
+	noeudUtilise.color = couleurs[type]
+	noeudUtilise.texture = itex[type]
